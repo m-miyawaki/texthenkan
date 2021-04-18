87e19b472d07c6f412509d9a838e3511dfd4ffc6
miyawaki <mmt23360@gmail.com>
2021-04-18 17:59:26 +0900

    0.1.0

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,9 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
 	//kakouのオプション	1: toHankaku; 2: deleteKaigyo; 3: deleteKaigyoSpace;
-	//				   4: commaToTouten; 5: toutenToComma; 6:doubleKakko;
-	//				   7: toUpperCase; 8: toLowerCase
+	//					4: commaToTouten; 5: toutenToComma; 6:doubleKakko;
+	//					7: toUpperCase; 8: toLowerCase; 9: warekiSeireki; 
+	//					10: seirekiWareki
 	context.subscriptions.push(vscode.commands.registerCommand('texthenkan.toHankaku', () => {kakou(1);	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('texthenkan.deleteKaigyo', () => {kakou(2);}));
@@ -20,7 +21,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('texthenkan.toUpperCase', () => {kakou(7);}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('texthenkan.toLowerCase', () => {kakou(8);}));
-
+	context.subscriptions.push(vscode.commands.registerCommand('texthenkan.warekiSeireki', () => {kakou(9);}));
+	context.subscriptions.push(vscode.commands.registerCommand('texthenkan.seirekiWareki', () => {kakou(10);}));
 }
 
 
@@ -39,27 +41,28 @@ function kakou(option: number){
 		let text = document.getText(selection); 
 
 	//kakouのオプション	1: toHankaku; 2: deleteKaigyo; 3: deleteKaigyoSpace;
-	//				   4: commaToTouten; 5: toutenToComma; 6:doubleKakko;
-	//				   7: toUpperCase; 8: toLowerCase
-	
+	//					4: commaToTouten; 5: toutenToComma; 6:doubleKakko;
+	//					7: toUpperCase; 8: toLowerCase; 9: warekiSeireki; 
+	//					10: seirekiWareki
+		
 		if(option === 1){
-			text = chikan(toHankaku(text),/　/g,' ');
+			text = toHankaku(text).replace(/　/g,' ');
 		}
 
 		if(option === 2){
-			text = chikan(text,/[\r\n]/g,'');
+			text = text.replace(/[\r\n]/g,'');
 		}
 
 		if(option === 3){
-			text = chikan(text,/\s/g,'');
+			text = text.replace(/\s/g,'');
 		}
 
 		if(option === 4){
-			text = chikan(text,/，/g,'、');
+			text = text.replace(/，/g,'、');
 		}
 
 		if(option === 5){
-			text = chikan(text,/、/g,'，');
+			text = text.replace(/、/g,'，');
 		}
 
 		if(option === 6){
@@ -76,17 +79,72 @@ function kakou(option: number){
 			text = text.toLowerCase() ;
 		}
 
+		if(option === 9){
+			while(true){
+				let wareki = text.match(/(?<gengo>明[\s　]*治|大[\s　]*正|昭[\s　]*和|平[\s　]*成|令[\s　]*和)[\s　]*(?<year>[0-9０-９元]{1,2})(?<other>年[\s　]*[0-9０-９]{0,2}月?[\s　]*[0-9０-９]{0,2}日?)/);
+				if(wareki === null){break;}
+				if(wareki.groups === undefined){break;}
+				if(wareki.groups.year === '元'){var wYear = "1";}
+				else {var wYear = toHankaku(wareki.groups.year);}
+				if(wareki.groups.gengo.includes('明')){ var addNum  : number = 1867;}
+				else if (wareki.groups.gengo.includes('大')){addNum = 1911;}
+				else if (wareki.groups.gengo.includes('昭')){addNum = 1925;}
+				else if (wareki.groups.gengo.includes('平')){addNum = 1988;}
+				else {addNum = 2018;}
+				let sYear = addNum + Number(wYear);
+				let otherHankaku = toHankaku(wareki.groups.other);
+				text = text.replace(wareki[0],String(sYear)+otherHankaku);
+			}
+
+		}
+		if(option === 10){
+			while(true){
+				let seireki = text.match(/(?<year>[0-9０-９]{4})(年[\s　]*)(?<month>[0-9０-９]{0,2})(月?[\s　]*)(?<day>[0-9０-９]{0,2})日?/);
+				if(seireki === null){break;}
+				if(seireki.groups === undefined){break;}
+				let sNen = toHankaku(seireki.groups.year);
+				let sNenNum = Number(sNen);
+				if (seireki.groups.month === ''){
+					var tsuki = '';
+					var tsukiNum = 6;
+				}
+				else {
+					tsuki = toHankaku(seireki.groups.month);
+					tsukiNum = Number(tsuki);
+				}
+				if (seireki.groups.day === ''){
+					var nichi = '';
+					var nichiNum = 15;
+				}
+				else {
+					nichi = toHankaku(seireki.groups.day);
+					nichiNum = Number(nichi);
+				}
+				var date = new Date(sNenNum, tsukiNum -1 ,nichiNum, 0,0, 0);
+				var waDate = new Intl.DateTimeFormat('ja-JP-u-ca-japanese', {era: 'long'}).format(date);
+				let splits = waDate.split(/[年月]/);
+				if (tsuki === ''){
+					var string = splits[0] +  seireki[2];
+				}
+				else {
+					if (nichi === ''){
+						string = splits[0] + seireki[2] + splits[1] + seireki[4];
+					}
+					else {
+						string = splits[0] + seireki[2] + splits[1] + seireki[4] + splits[2] ;
+					}
+
+				}
+				text = text.replace(seireki[0],string);
+			}
+
+		}
 		editor.edit(editBuilder => {
 			editBuilder.replace(selection, text);
 			});
 	}
 
 
-}
-
-function chikan(string: string, regex1: RegExp, regex2: string){
-	let result = string.replace(regex1, regex2);
-	return result;
 }
 
 function toHankaku(input: string) {
